c443459fa6f7d3096018164c11e74f18c5d454d3
Luka <lukawolf.dev@gmail.com>
2026-01-06 19:25:13 -0300

    Tailwindcss

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,11 +6,16 @@ export function Layout({
   zustandChildren: React.ReactElement;
 }) {
   return (
-    <main>
-      <section>
-        <h1>react context vs zustand</h1>
-        <p>Description</p>
-        <i className="fa-solid fa-user"></i>
+    <main className="max-w-300 w-full m-auto">
+      <section className="py-4 max-w-200 m-auto">
+        <h1 className="text-6xl font-bold capitalize text-center">
+          react context vs zustand
+        </h1>
+        <p className="mt-8">Description</p>
+      </section>
+      <section className="border bg-black/50 p-2 rounded-sm">
+        <div>{reactChildren}</div>
+        <div>{zustandChildren}</div>
       </section>
     </main>
   );
---
+++ src/components/templates/Main.tsx
@@ -0,0 +1,9 @@
+import type { AppReadContextType } from "../../types/app-context.type";
+
+export function MainTemplate({
+  fontSize,
+  theme: { background, text },
+  user: { avatar, name },
+}: AppReadContextType) {
+  return <></>;
+}
